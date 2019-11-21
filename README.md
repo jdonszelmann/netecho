df19e79df8b99dc5f0c9e4856a4e9c6ab418ec8a
Jonathan Donszelmann <jonabent@gmail.com>
2019-11-21 19:51:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+# netecho
+
+This program logs any request with any data it gets. 
+Useful for example for retrieving large amounts of data from servers with limited access to stdout/stderr but do have network access.
+
+# usage: 
+
+without reverse proxy:
+`echo "data" | curl  -F 'f=<-' mysubdomain.domain.com`
+
+with reverse proxy but no dns access:
+`echo "data" | curl --header 'Host: mysubdomain.domain.com' -F 'f=<-' IP address`
+
+
+To write directly to stdout, post to `/stdout` or `/stderr` 
---
+++ netecho.py
@@ -0,0 +1,59 @@
+import cgi
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import argparse
+import sys
+from datetime import datetime
+
+def get_data(obj):
+    # It's a huge hack that mostly comes from python2 libraries
+    # but sofar it's the easiest way I found to parse multipart
+    # formdata.
+    ctype, pdict = cgi.parse_header(obj.headers.get('content-type'))
+    pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+    pdict['CONTENT-LENGTH'] = int(obj.headers.get('Content-length'))
+
+    if ctype == 'multipart/form-data':
+        data = "\n".join((
+            " ".join(i) if isinstance(i, list) else str(i) for i in cgi.parse_multipart(obj.rfile, pdict).values()
+        ))
+    else:
+        data = obj.rfile.read(int(obj.headers.get('Content-length')))
+
+    return data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This program logs any request with any data it gets.");
+
+    parser.add_argument("-d", "--dir", type=str, help="Directory to write requests to.", default="logs")
+    parser.add_argument("-p", "--port", type=int, help="File to write data to.", default=8000)
+    args = parser.parse_args(sys.argv[1:])
+
+    if not os.path.exists(args.dir):
+        os.mkdir(args.dir)
+
+    symlinkpath = os.path.join(os.path.abspath(os.path.join(args.dir, "latest")))
+
+    if os.path.exists(symlinkpath):
+        os.unlink(symlinkpath)
+
+    filepath = os.path.join(args.dir, f"{datetime.now().strftime('%Y-%m-%d-%H:%M:%S')}.log")
+    logfile = open(filepath, "a", buffering=1)
+    os.symlink(os.path.abspath(filepath), symlinkpath)
+
+    class Handler(BaseHTTPRequestHandler):
+        def do_POST(self):
+            self.send_response(200)
+            self.end_headers()
+
+            if self.path == "/" or self.path == "":
+                print(get_data(self), file=logfile)
+            elif self.path == "/stdout":
+                print(get_data(self), file=sys.stdout)
+            elif self.path == "/stderr":
+                print(get_data(self), file=sys.stderr)
+
+
+with HTTPServer(('localhost', args.port), Handler) as httpd:
+    httpd.serve_forever()
